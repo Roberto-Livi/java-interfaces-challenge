a335888a30022e7bfe6cd96d1162c687f3fa1321
Roberto-Livi <robertomlivi@outlook.com>
2020-11-01 19:13:11 -0500

    testing methods in Main method

--- challenge/src/com/company/Main.java
+++ challenge/src/com/company/Main.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Player tim = new Player("Tim", 10, 15);
+        System.out.println(tim.toString());
+        saveObject(tim);
+
+        tim.setHitPoints(8);
+        System.out.println(tim);
+        tim.setWeapon("Stormbringer");
+        saveObject(tim);
+        loadObject(tim);
+        System.out.println(tim);
 
     }
 
@@ -35,5 +45,16 @@ public class Main {
             }
         }
         return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for(int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
     }
 }
